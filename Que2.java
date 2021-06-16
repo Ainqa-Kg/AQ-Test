d104bdcc1ec69ec96d9a9f8ff761ebb584c1c855
Sathish Rakin <sathish.rakin@ainqa.com>
2021-06-16 15:27:30 +0530

    fourth commit

---
+++ Que2.java
@@ -0,0 +1,31 @@
+package sathish;
+
+import java.util.Scanner;
+
+public class Que2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter the input: ");
+		Scanner sc = new Scanner(System.in);
+		int nVal = sc.nextInt();
+		
+	
+		for (int i=1;i<=nVal; i++)
+		{
+			for (int j=nVal;j>=1;j--)
+			{
+				if(i>=j)
+				{
+					System.out.print(" *");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+}
+}
---
+++ Que5.java
@@ -0,0 +1,25 @@
+package sathish;
+
+import java.util.Scanner;
+
+public class Que5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter the input: ");
+		Scanner sc = new Scanner(System.in);
+		int nVal = sc.nextInt();
+		
+		for (int i = 1; i <= nVal; i++)
+        {
+            for (int j = nVal; j >= i; j--)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+		
+	}
+
+}
